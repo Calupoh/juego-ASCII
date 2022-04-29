8f796c6fe6ec76e02fe5128a8fda52f6a5014300
Marco <m.moreno@calupoh.mx.nf>
2022-04-29 15:05:28 -0500

    ajustando funciones clase personaje

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ from msilib.schema import Class
 from tkinter import *
 from tkinter import ttk
 
-from requests import options
-
-size_x = 15
-size_y = 15
-
-
-def ajustar_cuadricula(col=30, row=30):
-    size_x = view['width']/col
-    size_y = view['height']/row
-
 
 class personaje():
-    def __init__(self, padre=None, npc=False, posicion=(0,0), size=(15,15)):
+    def __init__(
+        self, 
+        padre=None, 
+        npc=False, 
+        posicion=(0,0), 
+        size=(casilla_sx,casilla_sy)
+    ):
         self.sizex = size[0]
         self.sizey = size[1]
         self.posx = posicion[0]
@@ -28,23 +24,56 @@ class personaje():
         match direccion:
             case 'Right':
                 self.posx = self.posx + self.sizex
+                if self.posx > num_cols * casilla_sx:
+                    self.posx = (num_cols * casilla_sx) - casilla_sx
             case 'Left':
                 self.posx = self.posx - self.sizex
+                if self.posx < 0:
+                    self.posx = 0
             case 'Up':
                 self.posy = self.posy - self.sizey
+                if self.posy < 0:
+                    self.posy = 0
             case 'Down':
                 self.posy = self.posy + self.sizey
-        self.dibujar()
+                if self.posy > num_rows * casilla_sy:
+                    self.posy = (num_rows * casilla_sy) - casilla_sy
     
 
-    def dibujar(self):
-        self.camara.create_rectangle(
-                    self.posx,
-                    self.posy,
-                    self.posx + self.sizex,
-                    self.posy + self.sizey,
-                    fill='green'
-                )
+    def get_ubicacion(self):
+         return (
+                self.posx,
+                self.posy,
+                self.posx + self.sizex,
+                self.posy + self.sizey,
+                'green'
+         )
+
+
+class terreno():
+    pass
+
+
+def ajustar_cuadricula(col=30, row=30):
+    global casilla_sx
+    global casilla_sy
+    global num_cols
+    global num_rows
+    num_cols = col
+    num_rows = row
+    casilla_sx = view['width']/col
+    casilla_sy = view['height']/row
+
+
+def dibujar():
+    j = jugador.get_ubicacion()
+    view.create_rectangle(
+                j[0],
+                j[1],
+                j[2],
+                j[3],
+                fill=j[4]
+            )
 
 
 root = Tk()
@@ -56,9 +85,9 @@ view = Canvas(root, width=450, height=450, background='black')
 view.grid(column=0, row=0)
 
 jugador = personaje(view)
-root.bind('<Right>', lambda e:[jugador.movimiento(e.keysym)])
-root.bind('<Left>', lambda e:[jugador.movimiento(e.keysym)])
-root.bind('<Up>', lambda e:[jugador.movimiento(e.keysym)])
-root.bind('<Down>', lambda e:[jugador.movimiento(e.keysym)])
+root.bind('<Right>', lambda e:[jugador.movimiento(e.keysym), dibujar()])
+root.bind('<Left>', lambda e:[jugador.movimiento(e.keysym), dibujar()])
+root.bind('<Up>', lambda e:[jugador.movimiento(e.keysym), dibujar()])
+root.bind('<Down>', lambda e:[jugador.movimiento(e.keysym), dibujar()])
 
 root.mainloop()
